41a4472e25e075f0c9896a6cb4ff28183e7198a8
vThaian <b.plociennik@merixstudio.com>
2019-02-26 18:59:58 +0100

    Restore $ for thos urls

--- aldryn_newsblog/urls.py
+++ aldryn_newsblog/urls.py
@@ -9,7 +9,7 @@ from aldryn_newsblog.views import (
 
 
 urlpatterns = [
-    url(r'^', ArticleList.as_view(), name='article-list'),
+    url(r'^$', ArticleList.as_view(), name='article-list'),
     url(r'^feed/', LatestArticlesFeed(), name='article-list-feed'),
 
     url(r'^search/',
@@ -31,7 +31,7 @@ urlpatterns = [
     url(r'^(?P<year>\d{4})/(?P<month>\d{1,2})/(?P<day>\d{1,2})/(?P<pk>\d+)/',
         ArticleDetail.as_view(), name='article-detail'),
     # These support permalinks with <article_slug>
-    url(r'^(?P<slug>\w[-\w]*)/',
+    url(r'^(?P<slug>\w[-\w]*)/$',
         ArticleDetail.as_view(), name='article-detail'),
     url(r'^(?P<year>\d{4})/(?P<slug>\w[-\w]*)/',
         ArticleDetail.as_view(), name='article-detail'),
